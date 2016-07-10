667da21eeb1edc67bdfa1e39a55d5bcec2e17536
Raido Pahtma <raidopahtma@gmail.com>
2016-07-10 21:07:02 +0300

    Use NTP as time source, eliminating bad local clock issues.

--- cmd/sendrtc/sendrtc.go
+++ cmd/sendrtc/sendrtc.go
@@ -1,6 +1,7 @@
 // Author  Raido Pahtma
 // License MIT
 
+// sendrtc executable
 package main
 
 import "fmt"
@@ -15,7 +16,7 @@ import "github.com/proactivity-lab/go-rtcsync"
 import "github.com/jessevdk/go-flags"
 
 const ApplicationVersionMajor = 0
-const ApplicationVersionMinor = 1
+const ApplicationVersionMinor = 3
 const ApplicationVersionPatch = 0
 
 var ApplicationBuildDate string
@@ -23,14 +24,15 @@ var ApplicationBuildDistro string
 
 type Options struct {
 	Positional struct {
-		Host string
-		Port uint16
-	} `positional-args:"yes" required:"yes"  description:"Host and port"`
+		ConnectionString string `description:"Connectionstring sf@HOST:PORT"`
+	} `positional-args:"yes"`
 
 	Destination sfconnection.AMAddr `short:"d" long:"destination" default:"FFFF" description:"Sync destination"`
 
-	Address sfconnection.AMAddr  `short:"a" long:"address" default:"0015" description:"Local address"`
+	Address sfconnection.AMAddr  `short:"a" long:"address" default:"0001" description:"Local address"`
 	Group   sfconnection.AMGroup `short:"g" long:"group"   default:"22"   description:"Packet AM Group (hex)"`
+
+	NtpHost string `short:"n" long:"ntp-host" default:"0.pool.ntp.org" description:"NTP server address"`
 
 	Offset int64 `short:"o" long:"offset" default:"0"   description:"Time offset"`
 	Period int   `short:"p" long:"period" default:"900" description:"Announcement period"`
@@ -40,8 +42,15 @@ type Options struct {
 }
 
 func mainfunction() int {
+
 	var opts Options
 	opts.ShowVersion = func() {
+		if ApplicationBuildDate == "" {
+			ApplicationBuildDate = "YYYY-mm-dd_HH:MM:SS"
+		}
+		if ApplicationBuildDistro == "" {
+			ApplicationBuildDistro = "unknown"
+		}
 		fmt.Printf("sendrtc %d.%d.%d (%s %s)\n", ApplicationVersionMajor, ApplicationVersionMinor, ApplicationVersionPatch, ApplicationBuildDate, ApplicationBuildDistro)
 		os.Exit(0)
 	}
@@ -58,11 +67,17 @@ func mainfunction() int {
 		return 0
 	}
 
+	host, port, err := sfconnection.ParseSfConnectionString(opts.Positional.ConnectionString)
+	if err != nil {
+		fmt.Printf("ERROR: %s\n", err)
+		return 1
+	}
+
 	signals := make(chan os.Signal)
 	signal.Notify(signals, os.Interrupt, os.Kill)
 
 	sfc := sfconnection.NewSfConnection()
-	ss := rtcsync.NewSyncSender(sfc, opts.Address, opts.Group)
+	ss := rtcsync.NewSyncSender(sfc, opts.Address, opts.Group, opts.NtpHost)
 
 	logger := loggers.BasicLogSetup(len(opts.Debug))
 	if len(opts.Debug) > 0 {
@@ -70,7 +85,7 @@ func mainfunction() int {
 	}
 	ss.SetLoggers(logger)
 
-	sfc.Autoconnect(opts.Positional.Host, opts.Positional.Port, 30*time.Second)
+	sfc.Autoconnect(host, port, 30*time.Second)
 
 	time.Sleep(time.Second)
 
@@ -81,8 +96,9 @@ func mainfunction() int {
 		case sig := <-signals:
 			signal.Stop(signals)
 			logger.Debug.Printf("signal %s\n", sig)
-			ss.Exit <- true
+			sfc.Disconnect()
 			interrupted = true
+			ss.Exit <- true
 		}
 	}
 
--- rtcsync.go
+++ rtcsync.go
@@ -8,6 +8,7 @@ import "time"
 
 import "github.com/proactivity-lab/go-loggers"
 import "github.com/proactivity-lab/go-sfconnection"
+import "github.com/beevik/ntp"
 
 const AMID_RTC = 0x80
 
@@ -30,10 +31,12 @@ type SyncSender struct {
 	sfc *sfconnection.SfConnection
 	dsp *sfconnection.MessageDispatcher
 
+	host string
+
 	Exit chan bool
 }
 
-func NewSyncSender(sfc *sfconnection.SfConnection, source sfconnection.AMAddr, group sfconnection.AMGroup) *SyncSender {
+func NewSyncSender(sfc *sfconnection.SfConnection, source sfconnection.AMAddr, group sfconnection.AMGroup, host string) *SyncSender {
 	ss := new(SyncSender)
 	ss.InitLoggers()
 
@@ -42,24 +45,31 @@ func NewSyncSender(sfc *sfconnection.SfConnection, source sfconnection.AMAddr, g
 	ss.sfc = sfc
 	ss.sfc.AddDispatcher(ss.dsp)
 
+	ss.host = host
+
 	ss.Exit = make(chan bool)
 
 	return ss
 }
 
 func (self *SyncSender) AnnounceTime(destination sfconnection.AMAddr, offset int64) {
-	m := new(RTCSyncMsg)
-	m.Header = TIME_ANNOUNCEMENT_UNIX
-	m.Stratum = 5 // Random
-	m.Nxtime = time.Now().Unix() + offset
+	if ntpr, err := ntp.Query(self.host, 4); err == nil {
+		self.Debug.Printf("NTP %d stratum %d RTT %s offset %s", ntpr.Time.Unix(), ntpr.Stratum, ntpr.RTT, ntpr.ClockOffset)
+		m := new(RTCSyncMsg)
+		m.Header = TIME_ANNOUNCEMENT_UNIX
+		m.Stratum = ntpr.Stratum + 1
+		m.Nxtime = ntpr.Time.Unix() + offset
 
-	msg := self.dsp.NewMessage()
-	msg.SetDestination(destination)
-	msg.SetType(AMID_RTC)
-	msg.Payload = sfconnection.SerializePacket(m)
+		msg := self.dsp.NewMessage()
+		msg.SetDestination(destination)
+		msg.SetType(AMID_RTC)
+		msg.Payload = sfconnection.SerializePacket(m)
 
-	self.Info.Printf("Announce %s->%s %d(%d)\n", msg.Source(), destination, m.Nxtime, offset)
-	self.sfc.Send(msg)
+		self.Info.Printf("Announce %s->%s %d(%d)\n", msg.Source(), destination, m.Nxtime, offset)
+		self.sfc.Send(msg)
+	} else {
+		self.Warning.Printf("NTP query failed %s", err)
+	}
 }
 
 func (self *SyncSender) Run(destination sfconnection.AMAddr, period time.Duration, offset int64) {
