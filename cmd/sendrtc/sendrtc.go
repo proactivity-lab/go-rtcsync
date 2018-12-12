5e9def85409814aece369fa6e16e8b44ed6f2129
Raido Pahtma <raidopahtma@gmail.com>
2018-12-12 16:41:52 +0200

    Migrate to moteconnection, update NTP library, add quiet period.

--- cmd/sendrtc/sendrtc.go
+++ cmd/sendrtc/sendrtc.go
@@ -8,15 +8,17 @@ import "fmt"
 import "time"
 import "os"
 import "os/signal"
+import "strconv"
+import "strings"
 
 import "github.com/proactivity-lab/go-loggers"
-import "github.com/proactivity-lab/go-sfconnection"
+import "github.com/proactivity-lab/go-moteconnection"
 import "github.com/proactivity-lab/go-rtcsync"
 
 import "github.com/jessevdk/go-flags"
 
 const ApplicationVersionMajor = 0
-const ApplicationVersionMinor = 3
+const ApplicationVersionMinor = 4
 const ApplicationVersionPatch = 0
 
 var ApplicationBuildDate string
@@ -24,18 +26,19 @@ var ApplicationBuildDistro string
 
 type Options struct {
 	Positional struct {
-		ConnectionString string `description:"Connectionstring sf@HOST:PORT"`
+		ConnectionString string `description:"Connectionstring sf@HOST:PORT or serial@PORT:BAUD"`
 	} `positional-args:"yes"`
 
-	Destination sfconnection.AMAddr `short:"d" long:"destination" default:"FFFF" description:"Sync destination"`
-
-	Address sfconnection.AMAddr  `short:"a" long:"address" default:"0001" description:"Local address"`
-	Group   sfconnection.AMGroup `short:"g" long:"group"   default:"22"   description:"Packet AM Group (hex)"`
+	Group       moteconnection.AMGroup `short:"g" long:"group" default:"22" description:"Packet AM Group (hex)"`
+	Address     moteconnection.AMAddr  `short:"a" long:"address" default:"5678" description:"Source AM address (hex)"`
+	Destination moteconnection.AMAddr  `short:"d" long:"destination" default:"0" description:"Destination AM address (hex)"`
 
 	NtpHost string `short:"n" long:"ntp-host" default:"0.pool.ntp.org" description:"NTP server address"`
 
 	Offset int64 `short:"o" long:"offset" default:"0"   description:"Time offset"`
 	Period int   `short:"p" long:"period" default:"900" description:"Announcement period"`
+
+	Quiet []string `long:"quiet" description:"Quiet periods HH:MM-HH:MM"`
 
 	Debug       []bool `short:"D" long:"debug"   description:"Debug mode, print raw packets"`
 	ShowVersion func() `short:"V" long:"version" description:"Show application version"`
@@ -67,27 +70,56 @@ func mainfunction() int {
 		return 0
 	}
 
-	host, port, err := sfconnection.ParseSfConnectionString(opts.Positional.ConnectionString)
+	conn, cs, err := moteconnection.CreateConnection(opts.Positional.ConnectionString)
 	if err != nil {
 		fmt.Printf("ERROR: %s\n", err)
-		return 1
+		os.Exit(1)
 	}
 
 	signals := make(chan os.Signal)
 	signal.Notify(signals, os.Interrupt, os.Kill)
 
-	sfc := sfconnection.NewSfConnection()
-	ss := rtcsync.NewSyncSender(sfc, opts.Address, opts.Group, opts.NtpHost)
+	ss := rtcsync.NewSyncSender(conn, opts.Address, opts.Group, opts.NtpHost)
 
 	logger := loggers.BasicLogSetup(len(opts.Debug))
 	if len(opts.Debug) > 0 {
-		sfc.SetLoggers(logger)
+		conn.SetLoggers(logger)
 	}
 	ss.SetLoggers(logger)
 
-	sfc.Autoconnect(host, port, 30*time.Second)
+	for _, p := range opts.Quiet {
+		times := strings.Split(p, "-")
+		if len(times) != 2 {
+			fmt.Printf("Invalid quiet period %s\n", p)
+			os.Exit(1)
+		}
+
+		st := strings.Split(times[0], ":")
+		et := strings.Split(times[1], ":")
+		if len(st) != 2 || len(et) != 2 {
+			fmt.Printf("Invalid start or end time for quiet period %s\n", p)
+			os.Exit(1)
+		}
+
+		sth, err1 := strconv.Atoi(st[0])
+		stm, err2 := strconv.Atoi(st[1])
+		eth, err3 := strconv.Atoi(et[0])
+		etm, err4 := strconv.Atoi(et[1])
+
+		if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
+			fmt.Printf("Invalid start or end time for quiet period %s\n", p)
+			os.Exit(1)
+		}
+
+		ss.AddQuietPeriod(rtcsync.ClockTimePeriod{sth*3600 + stm*60, eth*3600 + etm*60})
+	}
+
+	conn.Autoconnect(30 * time.Second)
 
 	time.Sleep(time.Second)
+	if conn.Connected() {
+		logger.Info.Printf("Connected with %s\n", cs)
+	}
 
 	go ss.Run(opts.Destination, time.Duration(opts.Period)*time.Second, opts.Offset)
 
@@ -96,7 +128,7 @@ func mainfunction() int {
 		case sig := <-signals:
 			signal.Stop(signals)
 			logger.Debug.Printf("signal %s\n", sig)
-			sfc.Disconnect()
+			conn.Disconnect()
 			interrupted = true
 			ss.Exit <- true
 		}
--- rtcsync.go
+++ rtcsync.go
@@ -7,7 +7,7 @@ import "fmt"
 import "time"
 
 import "github.com/proactivity-lab/go-loggers"
-import "github.com/proactivity-lab/go-sfconnection"
+import "github.com/proactivity-lab/go-moteconnection"
 import "github.com/beevik/ntp"
 
 const AMID_RTC = 0x80
@@ -25,25 +25,32 @@ func (self *RTCSyncMsg) String() string {
 	return fmt.Sprintf("ta %d", self.Nxtime)
 }
 
+type ClockTimePeriod struct {
+	Start int
+	End   int
+}
+
 type SyncSender struct {
 	loggers.DIWEloggers
 
-	sfc *sfconnection.SfConnection
-	dsp *sfconnection.MessageDispatcher
+	conn moteconnection.MoteConnection
+	dsp  moteconnection.Dispatcher
 
 	host string
+
+	quiet []ClockTimePeriod
 
 	Exit chan bool
 }
 
-func NewSyncSender(sfc *sfconnection.SfConnection, source sfconnection.AMAddr, group sfconnection.AMGroup, host string) *SyncSender {
+func NewSyncSender(conn moteconnection.MoteConnection, source moteconnection.AMAddr, group moteconnection.AMGroup, host string) *SyncSender {
 	ss := new(SyncSender)
 	ss.InitLoggers()
 
-	ss.dsp = sfconnection.NewMessageDispatcher(sfconnection.NewMessage(group, source))
+	ss.dsp = moteconnection.NewMessageDispatcher(moteconnection.NewMessage(group, source))
 
-	ss.sfc = sfc
-	ss.sfc.AddDispatcher(ss.dsp)
+	ss.conn = conn
+	ss.conn.AddDispatcher(ss.dsp)
 
 	ss.host = host
 
@@ -52,36 +59,62 @@ func NewSyncSender(sfc *sfconnection.SfConnection, source sfconnection.AMAddr, g
 	return ss
 }
 
-func (self *SyncSender) AnnounceTime(destination sfconnection.AMAddr, offset int64) {
-	if ntpr, err := ntp.Query(self.host, 4); err == nil {
-		self.Debug.Printf("NTP %d stratum %d RTT %s offset %s", ntpr.Time.Unix(), ntpr.Stratum, ntpr.RTT, ntpr.ClockOffset)
-		m := new(RTCSyncMsg)
-		m.Header = TIME_ANNOUNCEMENT_UNIX
-		m.Stratum = ntpr.Stratum + 1
-		m.Nxtime = ntpr.Time.Unix() + offset
-
-		msg := self.dsp.NewMessage()
-		msg.SetDestination(destination)
-		msg.SetType(AMID_RTC)
-		msg.Payload = sfconnection.SerializePacket(m)
-
-		self.Info.Printf("Announce %s->%s %d(%d)\n", msg.Source(), destination, m.Nxtime, offset)
-		self.sfc.Send(msg)
-	} else {
-		self.Warning.Printf("NTP query failed %s", err)
-	}
+func (self *SyncSender) AddQuietPeriod(qp ClockTimePeriod) {
+	self.Debug.Printf("Quiet period %d-%d\n", qp.Start, qp.End)
+	self.quiet = append(self.quiet, qp)
 }
 
-func (self *SyncSender) Run(destination sfconnection.AMAddr, period time.Duration, offset int64) {
+func (self *SyncSender) AnnounceTime(destination moteconnection.AMAddr, ntpr *ntp.Response, offset int64) {
+	m := new(RTCSyncMsg)
+	m.Header = TIME_ANNOUNCEMENT_UNIX
+	m.Stratum = ntpr.Stratum + 1
+	m.Nxtime = ntpr.Time.Unix() + offset
+
+	msg := self.dsp.NewPacket().(*moteconnection.Message)
+	msg.SetDestination(destination)
+	msg.SetType(AMID_RTC)
+	msg.Payload = moteconnection.SerializePacket(m)
+
+	self.Info.Printf("Announce %s->%s %d(%d)\n", msg.Source(), destination, m.Nxtime, offset)
+	self.conn.Send(msg)
+}
+
+func (self *SyncSender) QuietPeriod(t time.Time) bool {
+	tu := t.UTC()
+	tss := tu.Hour()*3600 + tu.Minute()*60 + tu.Second()
+	self.Debug.Printf("Quiet period check: %d\n", tss)
+	for _, qp := range self.quiet {
+		if qp.Start <= tss && tss <= qp.End {
+			return true
+		}
+	}
+	return false
+}
+
+func (self *SyncSender) Run(destination moteconnection.AMAddr, period time.Duration, offset int64) {
 	self.Debug.Printf("run\n")
-	self.AnnounceTime(destination, offset)
+	announcePeriod := 1 * time.Second
 	for {
 		select {
 		case <-self.Exit:
 			self.Debug.Printf("Exit.\n")
-			self.sfc.Disconnect()
-		case <-time.After(period):
-			self.AnnounceTime(destination, offset)
+			self.conn.Disconnect()
+		case <-time.After(announcePeriod):
+			announcePeriod = period
+			if ntpr, err := ntp.Query(self.host); err == nil {
+				if err := ntpr.Validate(); err == nil {
+					self.Debug.Printf("NTP %d stratum %d RTT %s offset %s", ntpr.Time.Unix(), ntpr.Stratum, ntpr.RTT, ntpr.ClockOffset)
+					if self.QuietPeriod(ntpr.Time) {
+						self.Info.Printf("Quiet period, not sending time sync.\n")
+					} else {
+						self.AnnounceTime(destination, ntpr, offset)
+					}
+				} else {
+					self.Warning.Printf("NTP response validation failed %s", err)
+				}
+			} else {
+				self.Warning.Printf("NTP query failed %s", err)
+			}
 		}
 	}
 }
